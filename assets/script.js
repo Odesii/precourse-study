0c02e860efe630efc6709c5a66c6774c5d91006d
Odesii <alic.madd@gmail.com>
2024-03-11 18:40:33 -0400

    Add JavaScript functionality and update CSS and HTML files
    - Create a new JavaScript file (assets/script.js) to add dynamic functionality
    - Modify Page2.html to include new features or changes
    - Update the CSS file (assets/style.css) to enhance the styling of the page

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if (topics === "HTML") {
+    console.log("I'm learning HTML");
+} else if (topics === "CSS") {
+    console.log("I'm learning CSS");
+} else if (topics === "Git") {
+    console.log("I'm learning Git");
+}   else if (topics === "JavaScript") {
+    console.log("I'm learning JavaScript");
+} else {
+    console.log("You've Angered the Machines!");
+}
+*/
